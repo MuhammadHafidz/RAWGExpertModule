062e318b9c37a7c50047d28b3391367a803f4390
MuhammadHafidz <hafidzamarul@gmail.com>
2022-05-23 20:17:15 +0700

    fix ios version

--- RawgExpert/RawgExpertTests/RawgExpertTests.swift
+++ RawgExpert/RawgExpertTests/RawgExpertTests.swift
@@ -6,16 +6,34 @@
 //
 
 import XCTest
+import GameDomain
+import RxSwift
+import Alamofire
+import Core
+import GameRepository
 
 @testable import RawgExpert
 class RawgExpertTests: XCTestCase {
-
+    let disposeBag = DisposeBag()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testGetGames() throws {
+        let remote = GetGames()
+        
+        let res = remote.execute(request: nil)
+        
+        res.observe(on: MainScheduler.instance)
+            .subscribe { result in
+                XCTAssert(result.count > 0, "Game Not Found")
+            } onError: { error in
+                XCTAssertThrowsError("Error \(error.localizedDescription)")
+            }.disposed(by: disposeBag)
     }
 
     func testPerformanceExample() throws {
